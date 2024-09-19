679b1545e65c76eac59f8ecec2195a698a6fa5b7
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-09-19 18:57:30 +0500

    :heart: practice 2023

--- 2023/02.cheeseburger_corollary_2/solution.cpp
+++ 2023/02.cheeseburger_corollary_2/solution.cpp
@@ -1,3 +1,4 @@
+// https://www.facebook.com/codingcompetitions/hacker-cup/2023/practice-round/problems/A2
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -25,16 +26,29 @@ int main()
 	// Process each test case
 	for (int t = 1; t <= testCases; ++t)
 	{
-		int a, b, c;
+		long a, b, c;
 		// Reading input
 		inputFile >> a >> b >> c;
 
 		// Example computation (you will replace this with actual logic)
 
-		// <your code here...>
+		long single_burger_price = a, double_burger_price = b, cash = c;
+		long result = 0;
+
+		result = cash / single_burger_price; // buy all single burgers
+
+		// Double Burger Buy (Suppose Single Buy)
+		result = max(result, 2 * (cash / double_burger_price) - 1);
+
+		// 1 Single + Double Remaining
+		if (cash > single_burger_price)
+			result = max(result, 2 * ((cash - single_burger_price) / double_burger_price) + 1);
+
+		if (cash > 2 * single_burger_price)
+			result = max(result, 2 * ((cash - 2 * single_burger_price) / double_burger_price) + 2);
 
 		// Write the output (example format: Case #t: result)
-		outputFile << "Case #" << t << ": " << "YES" << endl;
+		outputFile << "Case #" << t << ": " << result << endl;
 	}
 
 	// Close the files
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Meta Hacker Cup
 
 This is Meta's annual open `programming competition`. Open to participants around the world, we invite you to apply problem-solving and algorithmic coding skills to advance through each year’s online rounds, win prizes, and have a chance to make it to the global finals and win the grand prize.
+
+```bash
+g++ solution.cpp -o compiled &&  ./compiled
+```
